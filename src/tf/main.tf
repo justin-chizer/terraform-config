50047caa7d6f9b1c3b16127015473bd11d8a6b2e
Justin Chizer <justin.chizer@gmail.com>
2020-05-21 16:23:32 -0700

    fmt will fail

--- src/tf/main.tf
+++ src/tf/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "hub" {
 
 resource "azurerm_network_security_group" "hub" {
   name                = var.hub_nsg_name
-  location            = azurerm_resource_group.hub.location
+  location   = azurerm_resource_group.hub.location
   resource_group_name = azurerm_resource_group.hub.name
 
   #   security_rule {
