0e3bbb026f4b5c7c5e9caff04633ca63371a7c91
Justin Chizer <justin.chizer@gmail.com>
2020-05-22 02:59:31 -0700

    prevent destory

--- src/tf/01-hub/hub.tf
+++ src/tf/01-hub/hub.tf
@@ -1,3 +1,17 @@
+data "terraform_remote_state" "hub" {
+  backend = "azurerm"
+
+  config = {
+    resource_group_name  = "avid-hub"
+    storage_account_name = "chizerstorageaccount"
+    container_name       = "tfbackend"
+    key                  = "prod.terraform.tfstate"
+    subscription_id      = var.subscription_id
+    client_id            = var.client_id
+    client_secret        = var.client_secret
+    tenant_id            = var.tenant_id
+  }
+}
 data "azurerm_resource_group" "hub" {
   name = "avid-hub"
 }
