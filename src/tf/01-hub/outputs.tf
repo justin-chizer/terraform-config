c3f7604243782dd883a0e87e6266703e1f7d6e8a
Justin Chizer <justin.chizer@gmail.com>
2020-05-22 03:58:57 -0700

    outputs

--- src/tf/01-hub/outputs.tf
+++ src/tf/01-hub/outputs.tf
@@ -1,7 +1,7 @@
 output "virtual_network_name" {
-  value = azurerm_virtual_network.hub.name
+  value = azurerm_virtual_network.core.name
 }
 
 output "virtual_network_id" {
-  value = azurerm_virtual_network.hub.id
+  value = azurerm_virtual_network.core.id
 }
