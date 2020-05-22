023c4c9cf995e7e8b9a3c19eeeab775fc4367630
Justin Chizer <justin.chizer@gmail.com>
2020-05-22 03:47:01 -0700

    still fail

--- src/tf/01-hub/hub.tf
+++ src/tf/01-hub/hub.tf
@@ -73,3 +73,4 @@ resource "azurerm_subnet" "core" {
   resource_group_name  = azurerm_resource_group.hub.name
   virtual_network_name = azurerm_virtual_network.hub.name
   address_prefixes     = ["10.0.1.0/24"]
+}
